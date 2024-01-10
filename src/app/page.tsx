f6a034f2828a162e9cb9af1bd79c38709435fa1d
RaffouIUT <rafa72don@gmail.com>
2024-01-10 10:25:54 +0100

    lien bdd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,23 @@
 import Image from 'next/image'
+import { sql } from "@vercel/postgres";
+
+export async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
+  const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
