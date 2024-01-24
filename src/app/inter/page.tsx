a11c752ed6721fc8a9b040789aac83a714779552
Alexis Guimbert <alexisguimbert16@gmail.com>
2024-01-24 20:38:45 +0100

    Fix intervenant choix session

--- src/app/inter/page.tsx
+++ src/app/inter/page.tsx
@@ -1,17 +1,12 @@
 import Session from "@/components/session";
 
 export default function Page() {
-    const liste = [];
-
-    for (let i = 0; i < 10; i++) {
-        liste.push(<Session/>)
-    }
 
     return (
         <div className={"text-justify"}>
             <div className={"my-8 text-6xl w-screen text-center"}>Séléction<p>conférence</p></div>
             <div className={"grid grid-cols-3"}>
-                {liste}
+                <Session/>
             </div>
         </div>
     );
--- src/components/session.tsx
+++ src/components/session.tsx
@@ -1,7 +1,17 @@
 export default function Session() {
-    return(
-        <div className={"flex bg-gray-300 rounded-lg mx-4 my-4 justify-center justify-self-center text-center w-5/6 h-5/6 text-2xl"}>
-           <button className={"w-full h-full"}> Conference NOM_COURS <br/> nom_inter <br/> 01/01/2024 <br/> 16h30-17h30 </button>
-        </div>
-    );
+    const liste = [];
+
+    {/* à changer pour chercher les cours du prof dans la bdd */}
+    for (let i = 0; i < 10; i++) {
+        liste.push(
+            <div
+                className={"flex bg-gray-300 rounded-lg mx-4 my-4 justify-center justify-self-center text-center w-5/6 h-5/6 text-2xl"}>
+                <button className={"w-full h-full"}> Conference
+                    NOM_COURS <br/> nom_inter <br/> 01/01/2024 <br/> 16h30-17h30
+                </button>
+            </div>
+        )
+    }
+
+    return (liste);
 }
