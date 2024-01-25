3bcb3afe2b3862194916f5eecbc08f0dbdf17a6b
Nicolas Giraud <giraud.nicolas36@gmail.com>
2024-01-25 16:47:39 +0100

    début page ajout des séances de cours avec plugin FullCalendar

--- src/app/admin/seance/page.tsx
+++ src/app/admin/seance/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <p>Dashboard Page</p>;
+import Calendar from "@/components/calendar";
+
+export default function Page()  {
+    return (
+        /* section gestion des séances du module intervenant */
+        <section className="flex flex-row">
+            {/* Menu latéral gauche, boutons d'actions */}
+            <div className={"basis-2/7 p-3 bg-sky-500/100 flex flex-col items-center"}>
+                <button className={"w-96 bg-amber-500 my-2"}>Gérer données présence</button>
+                <button className={"w-96 bg-amber-500 my-2"}>Paramètres</button>
+            </div>
+
+            {/* Partie centrale, calendrier des séances */}
+            <div className={"basis-5/7 p-3"}>
+                <Calendar />
+            </div>
+
+        </section>
+    );
 }
