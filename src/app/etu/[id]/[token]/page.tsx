e2f4e0ba22c49e50599d846f3f073a2cb9af54bd
Nicolas Giraud <giraud.nicolas36@gmail.com>
2024-04-05 19:04:05 +0200

    Inscription à un cours via le QR code / url

--- src/app/etu/[id]/[token]/page.tsx
+++ src/app/etu/[id]/[token]/page.tsx
@@ -1,29 +1,36 @@
 'use client'
-import { getTokenById } from '@/components/utils/coursUtils';
+import { getCoursById, getTokenById } from '@/components/utils/coursUtils';
 import { useEffect, useState } from 'react';
 import {redirectToCas, validateTicket} from "@/components/casLogin";
 import {useSearchParams} from "next/navigation";
+import { Cours } from '@prisma/client';
+import { CoursVide } from '@/components/utils/customTypes';
 
 export default function Page({ params }: {
     params: {id: string, token: string}
 }) {
     const [access, setAccess] = useState<boolean>(false);
 
+    const [cours, setCours] = useState<Cours>(structuredClone(CoursVide));
+
     const useParams = useSearchParams();
     useEffect(() => {
-        getTokenById(params.id).then((response) => {
-            if(response != null) {
-                if(response.tokenQrCode === params.token) {
-                    setAccess(true);
-                    const paramTicket = useParams.get("ticket");
-                    if (paramTicket == "" || paramTicket == null) {
-                        redirectToCas(params.id, params.token)
-                    } else {
-                        validateTicket(params.id, params.token, paramTicket);
+        getCoursById(params.id).then((coursU) => {
+            setCours(coursU)
+            getTokenById(params.id).then((response) => {
+                if(response != null) {
+                    if(response.tokenQrCode === params.token) {
+                        setAccess(true);
+                        const paramTicket = useParams.get("ticket");
+                        if (paramTicket == "" || paramTicket == null) {
+                            redirectToCas(params.id, params.token)
+                        } else {
+                            validateTicket(coursU, params.token, paramTicket);
+                        }
                     }
                 }
-            }
-        });
+            });
+        })
     }, []);
 
 
--- src/components/casLogin.ts
+++ src/components/casLogin.ts
@@ -1,20 +1,32 @@
 'use server';
 import { redirect, RedirectType } from 'next/navigation';
 import { DOMParser } from '@xmldom/xmldom';
+import { Cours } from '@prisma/client';
+import { getEtuById } from '@/components/utils/etuUtils';
+import { addInscription } from '@/components/utils/inscriptionsUtils';
 
 export async function redirectToCas(idSeance: string, idToken: string) {
     redirect(`https://cas-test.univ-lemans.fr/cas/login?service=http://umbriel.univ-lemans.fr/etu/${idSeance}/${idToken}`,RedirectType.push)
 }
 
-export async function validateTicket(idSeance: string, idToken: string, ticket: string) {
-    await fetch(`https://cas-test.univ-lemans.fr/cas/serviceValidate?ticket=${ticket}&service=http://umbriel.univ-lemans.fr/etu/${idSeance}/${idToken}`, {
+export async function validateTicket(cours: Cours, idToken: string, ticket: string) {
+    let uid: string = ""
+    let dateAuth: string = ""
+    await fetch(`https://cas-test.univ-lemans.fr/cas/serviceValidate?ticket=${ticket}&service=http://umbriel.univ-lemans.fr/etu/${cours.id}/${idToken}`, {
         method: "GET"
     })
         .then(r => r.text())
-        .then(str => new DOMParser().parseFromString(str, "text/xml"))
+        .then(str => new DOMParser().parseFromString(str, "application/xml"))
         .then(data => {
-            // le "17" correspond à l'UID, il faudra regarder manuellement pour les autres infos
-            // @ts-ignore
-            console.log("FUCKING UID", data.childNodes[0].childNodes[1].childNodes[3].childNodes[17].childNodes[0].data)
+            uid = "" + data.getElementsByTagName("cas:uid")[0]?.textContent;
+            dateAuth = "" + data.getElementsByTagName("cas:authenticationDate")[0]?.textContent;
         });
+
+    getEtuById(uid).then((etu) => {
+        if (etu && etu.promotionId == cours.promotionId) {
+            // On changera le present en fonction de l'heure
+            addInscription(cours.id, etu.id, "present")
+        }
+    })
+
 }
